a520053a146eca981a6ce4ba9f6770a469ffe662
Johann Levert <exes972@msn.com>
2023-01-13 06:13:46 -0500

    Breadcrumbs SubHeader

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import { Container } from "reactstrap";
 import DisplayLIst from "../features/display/DisplayList";
+import SubHeader from "../components/SubHeader";
 
 
 const HomePage =() => {
   return (
     <Container>
+      <SubHeader current='Home' />
       <DisplayLIst/>
     </Container>
   );
