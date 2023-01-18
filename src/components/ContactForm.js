0ab188fb37878d81a667f696124861ffc1686bb4
Johann Levert <exes972@msn.com>
2023-01-18 08:24:47 -0600

    ContactForm Part 3

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,7 +1,14 @@
 import { Button, Label, Col, FormGroup} from 'reactstrap';
 import {Formik, Field, Form } from 'formik';
 
+
 const ContactForm = () => {
+  const handleSubmit =(values, {resetForm}) => {
+    console.log('form values:', values);
+    console.log('in JSON format:', JSON.stringify(values));
+    resetForm();  
+  }
+
   return (
     <Formik 
       initialValues={{
@@ -13,6 +20,7 @@ const ContactForm = () => {
         contactType: 'By Phone',
         feedback: ''        
       }}
+      onSubmit={handleSubmit}
     >
       <Form>
         <FormGroup row>
@@ -98,9 +106,12 @@ const ContactForm = () => {
           </Col>           
         </FormGroup>
         <FormGroup row>
-          
-        </FormGroup>        
-
+          <Col md={{size:10, offset: 2}} >
+            <Button type='submit' color='primary'>
+              Send Feedback
+            </Button>
+          </Col>          
+        </FormGroup>
       </Form>
     </Formik>
   )
